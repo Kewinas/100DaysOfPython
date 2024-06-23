88985d18273459011f6923a8cf78344c73b6f777
Kewinas <aurimux.r@gmail.com>
2024-06-23 10:18:04 +0300

    Day 1 Completed

---
+++ Day1/day1.py
@@ -0,0 +1,12 @@
+# Print
+print("Hello world!")
+print("Hello world!\nHello world!")
+print("Hello" + " world!")
+print("Hello" + " " + "world!")
+
+# Print + Input
+print ("Hello " + input("What is your name?") + "!")
+
+# Variables
+name = input("What is your name?")
+print(name)
---
+++ Day1/day1Project.py
@@ -0,0 +1,6 @@
+# Band Name Generator
+
+print ("Sveiki atvykę į grupės pavadinimo generatorių!")
+city = input("Kokiame mieste užaugote?\n")
+pet = input("Koks jūsų augintinio vardas?\n")
+print("Jūsų grupės pavadinimas gali būti toks: " + city + " " + pet)
