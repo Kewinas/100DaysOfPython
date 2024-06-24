6bc69913c75a9a01e4c5c02324caf429c395b655
Kewinas <aurimux.r@gmail.com>
2024-06-24 11:26:37 +0300

    Day 5 Completed

--- Day5/average_height.py
+++ Day5/average_height.py
@@ -2,13 +2,14 @@ student_heights = input().split()
 for n in range(0, len(student_heights)):
     student_heights[n] = int(student_heights[n])
 
+average = 0
 count = 0
-sum = 0
+total = 0
 for height in student_heights:
     count += 1
-    sum += height
-    average = round(sum / count)
+    total += height
+    average = round(total / count)
 
-print(f"total height = {sum}")
+print(f"total height = {total}")
 print(f"number of students = {count}")
 print(f"average height = {average}")
