81461096e38c7ef4e7e22d41c128be99b1d1645c
Kewinas <aurimux.r@gmail.com>
2024-07-24 08:58:31 +0300

    Day 24 Completed

--- Days_20-29/Day_20-21(Snake)/scoreboard.py
+++ Days_20-29/Day_20-21(Snake)/scoreboard.py
@@ -1,6 +1,7 @@
 from turtle import Turtle
+
 ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 18, "normal")
 
 
 class Scoreboard(Turtle):
@@ -8,6 +9,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -15,13 +17,21 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+        self.clear()
+        with open("highscore.txt") as file:
+            self.high_score = int(file.read())
+        self.write(f"Score: {self.score} High Score: {self.high_score}",
+                   align=ALIGNMENT,
+                   font=FONT)
 
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+    def reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+            with open("highscore.txt", mode="w") as file:
+                file.write(str(self.high_score))
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
